1add61607679d10328d5a9bb947e047a1a34d0c8
Alexander <alexoneki3@gmail.com>
2023-03-13 21:43:54 -0300

    version-1

---
+++ js.js
@@ -0,0 +1,7 @@
+const menu_icon = document.getElementById(`menu-icon`);
+const menu = document.getElementById(`menu`);
+
+
+menu_icon.addEventListener(`click`, ()=>{
+    menu.classList.toggle(`menu--active`);
+})
